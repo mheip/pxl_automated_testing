5f9bfea6880e3c66704ffa6da1bb8d4d16fcbb66
Maarten Heip <maarten.heip@intracto.com>
2022-11-08 21:33:32 +0100

    Write tests that assert the results on our checkout page

--- laravel/tests/Feature/CheckoutTest.php
+++ laravel/tests/Feature/CheckoutTest.php
@@ -4,8 +4,12 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 
+/**
+ * Contains a feature integration test for the checkout.
+ */
 class CheckoutTest extends TestCase
 {
+
     /**
      * A basic test example.
      *
@@ -15,5 +19,71 @@ class CheckoutTest extends TestCase
     {
         $response = $this->get('/checkout');
         $response->assertStatus(200);
+    }
+
+    /**
+     * Tests the total price of the products.
+     *
+     * @return void
+     */
+    public function testTotalPrice()
+    {
+        $response = $this->get('/checkout');
+        $response->assertSee('2818');
+    }
+
+    /**
+     * Test the discount percentage.
+     *
+     * @return void
+     */
+    public function testDiscountPercentage()
+    {
+        $response = $this->get('/checkout');
+        $response->assertSee('5%');
+    }
+
+    /**
+     * Tests the discount amount.
+     *
+     * @return void
+     */
+    public function testDiscountAmount()
+    {
+        $response = $this->get('/checkout');
+        $response->assertSee('140.9');
+    }
+
+    /**
+     * Tests the total price without vat.
+     *
+     * @return void
+     */
+    public function testPriceWithoutVat()
+    {
+        $response = $this->get('/checkout');
+        $response->assertSee('2677.1');
+    }
+
+    /**
+     * Tests the amount of VAT to be added to the price.
+     *
+     * @return void
+     */
+    public function testVatAmount()
+    {
+        $response = $this->get('/checkout');
+        $response->assertSee('562.191');
+    }
+
+    /**
+     * Tests the price with VAT.
+     *
+     * @return void
+     */
+    public function testPriceWithVat()
+    {
+        $response = $this->get('/checkout');
+        $response->assertSee('3239.291');
     }
 }
