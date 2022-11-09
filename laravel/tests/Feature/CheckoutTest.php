9c97e7027fe2d35f0d935c63c9673af6da42a8e9
kenny.derwael <kenny.derwael@iodigital.com>
2022-11-09 09:59:29 +0100

    Update checkout test

--- laravel/tests/Feature/CheckoutTest.php
+++ laravel/tests/Feature/CheckoutTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Checkout;
+use App\Models\CheckoutProduct;
+use App\Models\Product;
 use Tests\TestCase;
 
 /**
@@ -9,67 +12,90 @@ use Tests\TestCase;
  */
 class CheckoutTest extends TestCase
 {
+    /**
+     * Checkout we want to test for global discount.
+     *
+     * @var \App\Models\Checkout
+     */
+    protected Checkout $checkoutWithDiscount;
 
     /**
-     * A basic test example.
+     * Checkout we want to test for no global discount.
+     *
+     * @var \App\Models\Checkout
      */
-    public function test_the_application_returns_a_successful_response(): void
+    protected Checkout $checkoutNoDiscount;
+
+    /**
+     * Setup our test.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $checkoutProducts = [
+            new CheckoutProduct(new Product('Monitor', 300), 2, 0.12),
+            new CheckoutProduct(new Product('GPU', 1300), 1, 0.20),
+            new CheckoutProduct(new Product('RAM 8GB', 75), 4),
+            new CheckoutProduct(new Product('NVMe 2GB', 120), 2),
+        ];
+        $this->checkoutNoDiscount = new Checkout($checkoutProducts);
+
+        $checkoutProducts[] = new CheckoutProduct(new Product('Moederbord', 275), 1);
+        $checkoutProducts[] = new CheckoutProduct(new Product('Voeding 1500W', 120), 1, 0.50);
+        $checkoutProducts[] = new CheckoutProduct(new Product('CPU', 375), 1);
+        $this->checkoutWithDiscount = new Checkout($checkoutProducts);
+    }
+
+    /**
+     * Test out view with a discount.
+     */
+    public function testCheckoutDiscountView(): void
+    {
+        $view = $this->view('checkout', [
+            'products' => $this->checkoutWithDiscount->getProducts(),
+            'totalPriceFull' => $this->checkoutWithDiscount->calculatePriceWithoutGlobalDiscount(),
+            'totalPrice' => $this->checkoutWithDiscount->calculatePriceWithoutVat(),
+            'btwAmount' => $this->checkoutWithDiscount->calculateVat(),
+            'globalDiscountPercentage' => $this->checkoutWithDiscount->getGlobalDiscountPercentage(),
+            'globalDiscountAmount' => $this->checkoutWithDiscount->calculateDiscount(),
+            'totalPriceInclBTW' => $this->checkoutWithDiscount->calculatePriceWithVat(),
+        ]);
+        $view->assertSee('2818');
+        $view->assertSee('5%');
+        $view->assertSee('140.9');
+        $view->assertSee('2677.1');
+        $view->assertSee('562.191');
+        $view->assertSee('3239.291');
+    }
+
+    /**
+     * Test out view with a discount.
+     */
+    public function testCheckoutWithoutDiscountView(): void
+    {
+        $view = $this->view('checkout', [
+            'products' => $this->checkoutWithDiscount->getProducts(),
+            'totalPriceFull' => $this->checkoutWithDiscount->calculatePriceWithoutGlobalDiscount(),
+            'totalPrice' => $this->checkoutWithDiscount->calculatePriceWithoutVat(),
+            'btwAmount' => $this->checkoutWithDiscount->calculateVat(),
+            'globalDiscountPercentage' => $this->checkoutWithDiscount->getGlobalDiscountPercentage(),
+            'globalDiscountAmount' => $this->checkoutWithDiscount->calculateDiscount(),
+            'totalPriceInclBTW' => $this->checkoutWithDiscount->calculatePriceWithVat(),
+        ]);
+        $view->assertSee('2818');
+        $view->assertSee('5%');
+        $view->assertSee('140.9');
+        $view->assertSee('2677.1');
+        $view->assertSee('562.191');
+        $view->assertSee('3239.291');
+    }
+
+    /**
+     * A basic test to see if the checkout works.
+     */
+    public function testCheckoutResponse(): void
     {
         $response = $this->get('/checkout');
         $response->assertStatus(200);
-    }
-
-    /**
-     * Tests the total price of the products.
-     */
-    public function testTotalPrice(): void
-    {
-        $response = $this->get('/checkout');
-        $response->assertSee('2818');
-    }
-
-    /**
-     * Test the discount percentage.
-     */
-    public function testDiscountPercentage(): void
-    {
-        $response = $this->get('/checkout');
-        $response->assertSee('5%');
-    }
-
-    /**
-     * Tests the discount amount.
-     */
-    public function testDiscountAmount(): void
-    {
-        $response = $this->get('/checkout');
-        $response->assertSee('140.9');
-    }
-
-    /**
-     * Tests the total price without vat.
-     */
-    public function testPriceWithoutVat(): void
-    {
-        $response = $this->get('/checkout');
-        $response->assertSee('2677.1');
-    }
-
-    /**
-     * Tests the amount of VAT to be added to the price.
-     */
-    public function testVatAmount(): void
-    {
-        $response = $this->get('/checkout');
-        $response->assertSee('562.191');
-    }
-
-    /**
-     * Tests the price with VAT.
-     */
-    public function testPriceWithVat(): void
-    {
-        $response = $this->get('/checkout');
-        $response->assertSee('3239.291');
     }
 }
